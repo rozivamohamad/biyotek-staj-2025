df60e4763dcc56a81bc9951bbfac7aeb6e37e8d8
rozivamohamad <rozivamohammat@gmail.com>
2025-04-16 07:25:31 +0300

    data review

--- codes/microarray-analiz.R
+++ codes/microarray-analiz.R
@@ -1,3 +1,12 @@
 library(GEOquery)
 
 txdata<-getGEO("GSE12345")
+
+
+exprs_data <- exprs(txdata)
+ # Ekspresyon matrisi
+pdata <- pData(txdata) 
+ # Fenotipik bilgiler (örneğin grup, hastalık durumu)
+head(txdata[,1:5])
+# İlk 5 örneğin ilk genleri
+head(pdata)
