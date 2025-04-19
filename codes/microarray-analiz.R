6bde257af2adaee481d15f86971c717ccdf5fa9e
rozivamohamad <rozivamohammat@gmail.com>
2025-04-19 22:45:25 +0300

    transposed and normalization data

--- codes/microarray-analiz.R
+++ codes/microarray-analiz.R
@@ -11,24 +11,9 @@ phenotx<-txdata@phenoData@data
 
 metatx<-txdata@featureData@data
 
-hippocampus_rows<-phenotx[grep("hippocampus",phenotx$characteristics_ch1.1),]
+transposed_data<-t(assaytx)
+hist(transposed_data)
 
-hippocampus_rows_AD<-phenotx[grep("hippocampus",phenotx$characteristics_ch1.2),]
+transposed_data_N<-log2(transposed_data)
 
-postcentral_gyrus_rows<-phenotx[grep("postcentral gyrus",phenotx$characteristics_ch1.1),]
-
-superior_frontal_gyrus_rows<-phenotx[grep("superior frontal gyrus",phenotx$characteristics_ch1.1),]
-
-entorhinal_cortex_rows<-phenotx[grep("entorhinal cortex",phenotx$characteristics_ch1.1),]
-
-
-united_hippocampus<-rbind(hippocampus_rows,hippocampus_rows_AD)
-
-hippocampus_data <- assaytx[, colnames(assaytx) %in% united_hippocampus$geo_accession]
-
-
-hist(hippocampus_data)
-
-hippocampus_data_N<-log2(hippocampus_data)
-
-hist(hippocampus_data_N)
+hist(transposed_data_N)
