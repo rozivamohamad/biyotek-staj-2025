84c7d51cd84a9101b6f5cb86f9ddb35dd49b7d4c
rozivamohamad <rozivamohammat@gmail.com>
2025-04-28 13:26:06 +0300

    coment edet

--- codes/microarray-analiz.R
+++ codes/microarray-analiz.R
@@ -1,19 +1,25 @@
+# veri okumak için paket yukleyelim 
 library(GEOquery)
 
+# verileri yukleyelim 
 txdata<-getGEO("GSE48350")
 
-
+# Listenin içerisindeki ilk verisetini inceleyelim 
 txdata<-txdata[[1]]
 
+#
 assaytx<-txdata@assayData$exprs
-
 phenotx<-txdata@phenoData@data
-
 metatx<-txdata@featureData@data
 
+# assaytx'teki sutunleri satır yapalım
 transposed_data<-t(assaytx)
+
+# histogram grafigi oluşturalım
 hist(transposed_data)
 
+# veriyi normalize edelim 
 transposed_data_N<-log2(transposed_data)
 
+# histogram grafigi olusturalım 
 hist(transposed_data_N)
