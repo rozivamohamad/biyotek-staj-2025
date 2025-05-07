b15ea287bbb0381995de6092d556072149c09775
rozivamohamad <rozivamohammat@gmail.com>
2025-05-07 21:41:04 +0300

    pca $ttest

--- codes/microarray-analiz.R
+++ codes/microarray-analiz.R
@@ -2,7 +2,7 @@
 library(GEOquery)
 
 # verileri yukleyelim 
-txdata<-getGEO("GSE48350")
+txdata<-getGEO("GSE50161")
 
 # Listenin içerisindeki ilk verisetini inceleyelim 
 txdata<-txdata[[1]]
@@ -12,14 +12,42 @@ assaytx<-txdata@assayData$exprs
 phenotx<-txdata@phenoData@data
 metatx<-txdata@featureData@data
 
-# assaytx'teki sutunleri satır yapalım
-transposed_data<-t(assaytx)
+#GSE50161 verisinden ependimoma ve kontrol gruplarini ayrilalim
+organisassay<-assaytx[c(1:46,103:115),]
+
+# veriyi normalize edelim 
+organisassay_N<-log2(organisassay)
+hist(organisassay_N)
+
+# organisassay'teki sutunleri satır yapalım
+transposed_data<-t(organisassay)
 
 # histogram grafigi oluşturalım
 hist(transposed_data)
 
-# veriyi normalize edelim 
-transposed_data_N<-log2(transposed_data)
+# veriyi PCA yapalım
+dataPCA<-prcomp(transposed_data)
+PC1<-dataPCA$x[,1]
+PC2<-dataPCA$x[ ,2]
 
-# histogram grafigi olusturalım 
-hist(transposed_data_N)
+plot(dataPCA$x)
+
+# t test yapalım 
+# once p degerlerini toplayacagimiz bir boş vektor oluşturalim
+
+p.value <- rep(0, nrow(transposed_data))
+
+for (i in 1:nrow(transposed_data)){
+  normal<- transposed_data[i,phenotx$source_name_ch1=="normal brain"]
+  tumor<- transposed_data[i, phenotx$source_name_ch1=="brain tumor"]
+  
+  test <- t.test(x = kontrol, y =tumor )
+  p.value[i] <- test$p.value
+}
+
+
+sum(p.value < 0.01, na.rm = TRUE)
+
+
+sum(p.value < 0.05, na.rm = TRUE)
+
