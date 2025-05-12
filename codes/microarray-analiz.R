894f35a894387b59ae52262290531e5e9478ea97
Berkay <berkay.demirsecen23@gmail.com>
2025-05-12 13:25:43 +0300

    codes updated

--- codes/microarray-analiz.R
+++ codes/microarray-analiz.R
@@ -13,7 +13,7 @@ phenotx<-txdata@phenoData@data
 metatx<-txdata@featureData@data
 
 #GSE50161 verisinden ependimoma ve kontrol gruplarini ayrilalim
-organisassay<-assaytx[c(1:46,103:115),]
+organisassay<-assaytx[,c(1:46,103:115)]
 
 # veriyi normalize edelim 
 organisassay_N<-log2(organisassay)
@@ -22,9 +22,6 @@ hist(organisassay_N)
 # organisassay'teki sutunleri satır yapalım
 transposed_data<-t(organisassay)
 
-# histogram grafigi oluşturalım
-hist(transposed_data)
-
 # veriyi PCA yapalım
 dataPCA<-prcomp(transposed_data)
 PC1<-dataPCA$x[,1]
@@ -32,22 +29,15 @@ PC2<-dataPCA$x[ ,2]
 
 plot(dataPCA$x)
 
+
+# ggplot(data=dataPCA$x, aes(x=PC1, y=PC2)) + geom_point()
 # t test yapalım 
 # once p degerlerini toplayacagimiz bir boş vektor oluşturalim
 
-p.value <- rep(0, nrow(transposed_data))
-
-for (i in 1:nrow(transposed_data)){
-  normal<- transposed_data[i,phenotx$source_name_ch1=="normal brain"]
-  tumor<- transposed_data[i, phenotx$source_name_ch1=="brain tumor"]
-  
-  test <- t.test(x = kontrol, y =tumor )
-  p.value[i] <- test$p.value
-}
+p_val=NULL
 
 
-sum(p.value < 0.01, na.rm = TRUE)
-
-
-sum(p.value < 0.05, na.rm = TRUE)
+for (i in 1:nrow(organisassay_N)) { 
+  p_val[i] = t.test(organisassay_N[i,1:46], organisassay_N[i,47:59])$p.value #Since t.test function gives more numbers then just the p.value, we should take the p-value by $p.value
+} #Constructed a for loop for every row of the assay data. 
 
