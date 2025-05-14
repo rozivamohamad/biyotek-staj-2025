877cb2ce2dac21f314285c2b10deca732265b65d
rozivamohamad <rozivamohammat@gmail.com>
2025-05-14 11:32:21 +0300

    flip genes names

--- codes/microarray-analiz.R
+++ codes/microarray-analiz.R
@@ -1,4 +1,4 @@
-# veri okumak için paket yukleyelim 
+# veri okumak için paket yukleyelim.
 library(GEOquery)
 
 # verileri yukleyelim 
@@ -13,41 +13,54 @@ phenotx<-txdata@phenoData@data
 metatx<-txdata@featureData@data
 
 #GSE50161 verisinden ependimoma ve kontrol gruplarini ayrilalim
-organisassay<-assaytx[c(1:46,103:115),]
+organisassay<-assaytx[,c(1:46,103:115)]
+
+#histogram grafigi olusturalim
+hist(organisassay)
+
+
 
 # veriyi normalize edelim 
-organisassay_N<-log2(organisassay)
+organisassay_N <-log2(organisassay)
 hist(organisassay_N)
 
 # organisassay'teki sutunleri satır yapalım
 transposed_data<-t(organisassay)
 
-# histogram grafigi oluşturalım
-hist(transposed_data)
+
 
 # veriyi PCA yapalım
 dataPCA<-prcomp(transposed_data)
 PC1<-dataPCA$x[,1]
 PC2<-dataPCA$x[ ,2]
 
-plot(dataPCA$x)
+
+labels<-row.names(transposed_data)
+ggplot(data = dataPCA$x, aes(x= PC1, y= PC2)) + geom_point() + geom_text(label=labels, check_overlap = T)
 
 # t test yapalım 
 # once p degerlerini toplayacagimiz bir boş vektor oluşturalim
 
-p.value <- rep(0, nrow(transposed_data))
+p_value <- NULL
 
-for (i in 1:nrow(transposed_data)){
-  normal<- transposed_data[i,phenotx$source_name_ch1=="normal brain"]
-  tumor<- transposed_data[i, phenotx$source_name_ch1=="brain tumor"]
+for (i in 1:nrow(organisassay_N)){
+
+  p_value[i]<-t.test(organisassay_N [i,1:46], organisassay_N [i,47:59])$p.value
   
-  test <- t.test(x = kontrol, y =tumor )
-  p.value[i] <- test$p.value
+  
 }
 
+BF_p_value<-p.adjust(p_value,method = "bonferroni")
 
-sum(p.value < 0.01, na.rm = TRUE)
+BF_sign_genes=which(BF_p_value<0.01)
+
+#gen isimleri geri dondurelim.
+genes_names<-metatx$`Gene Symbol`[BF_sign_genes]
+
+unique_genisimler<-unique(genes_names)
+
+##dondurdumuz gen isinleri bir txt yapalım.
+write.table(unique_genisimler,file = "unique gen isimleri.txt",sep="\t",quote = F,row.names = F)
 
 
-sum(p.value < 0.05, na.rm = TRUE)
 
